4567fa72f2eea54a098a03d768d652f30ff552a2
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2018-12-05 21:17:19 -0200

    Update travis and some classes

--- src/test/java/com/campos/pontointeligente/PontoInteligenteApplicationTests.java
+++ src/test/java/com/campos/pontointeligente/PontoInteligenteApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PontoInteligenteTests {
+public class PontoInteligenteApplicationTests {
 
 	@Test
 	public void contextLoads() {
