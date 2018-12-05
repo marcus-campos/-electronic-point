41848dfcef8c7699284e84e6b8416ad188273788
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2018-12-05 20:45:37 -0200

    Initial commit

---
+++ ponto-inteligente/src/main/java/com/campos/pontointeligente/PontoInteligenteApplication.java
@@ -0,0 +1,12 @@
+package com.campos.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ ponto-inteligente/src/test/java/com/campos/pontointeligente/PontoInteligenteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.campos.pontointeligente;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontoInteligenteTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
